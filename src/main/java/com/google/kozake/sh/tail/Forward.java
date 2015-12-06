670fa80b84692aa097b41eb9158916345034c7e8
kozake <sh.kozake@gmail.com>
2015-12-07 08:05:04 +0900

    rlineつくらねば〜

--- src/main/java/com/google/kozake/sh/tail/Forward.java
+++ src/main/java/com/google/kozake/sh/tail/Forward.java
@@ -1,12 +1,12 @@
 package com.google.kozake.sh.tail;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.channels.FileChannel;
 
 public final class Forward {
 
-    public static void forward(File file, FileChannel fileChannel, Style style, long off) throws IOException {
+    public static void forward(File file, FileInputStream fis, Style style, long off) throws IOException {
         int ch;
 
         switch(style) {
@@ -17,70 +17,38 @@ public final class Forward {
                 if (file.length() < off) {
                     off = file.length();
                 }
-                fileChannel.position(off);
+                fis.getChannel().position(off);
                 break;
             case FLINES:
                 if (off == 0)
                     break;
-                for (;;) {
-                    if ((ch = getc(fp)) == EOF) {
-                        if (ferror(fp)) {
-                            ierr(fn);
-                            return;
-                        }
+                for (; ; ) {
+                    if ((ch = fis.read()) == -1) {
                         break;
                     }
-                    if (ch == '\n' && !--off)
+                    if (ch == '\n' && --off == 0)
                         break;
                 }
                 break;
             case RBYTES:
-                if (S_ISREG(sbp->st_mode)) {
-                    if (sbp->st_size >= off &&
-                            fseeko(fp, -off, SEEK_END) == -1) {
-                        ierr(fn);
-                        return;
-                    }
-                } else if (off == 0) {
-                    while (getc(fp) != EOF);
-                    if (ferror(fp)) {
-                        ierr(fn);
-                        return;
-                    }
-                } else
-                if (bytes(fp, fn, off))
-                    return;
+                if (file.length() >= off) {
+                    fis.getChannel().position(file.length() - off);
+                }
                 break;
             case RLINES:
-                if (S_ISREG(sbp->st_mode))
-                    if (!off) {
-                        if (fseeko(fp, (off_t)0, SEEK_END) == -1) {
-                            ierr(fn);
-                            return;
-                        }
-                    } else
-                        rlines(fp, fn, off, sbp);
-                else if (off == 0) {
-                    while (getc(fp) != EOF);
-                    if (ferror(fp)) {
-                        ierr(fn);
-                        return;
-                    }
-                } else
-                if (lines(fp, fn, off))
-                    return;
+                if (off == 0) {
+                    fis.getChannel().position(file.length() - 1);
+                } else {
+                    rlines(fp, fn, off, sbp);
+                }
                 break;
             default:
                 break;
         }
 
-        while ((ch = getc(fp)) != EOF)
-            if (putchar(ch) == EOF)
-                oerr();
-        if (ferror(fp)) {
-            ierr(fn);
-            return;
+        while ((ch = fis.read()) != -1) {
+            System.out.write(ch);
         }
-        (void)fflush(stdout);
+        System.out.flush();
     }
 }
