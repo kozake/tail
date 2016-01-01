188fc12b35729719535efc90161b80b9d8ba0a69
kozake <sh.kozake@gmail.com>
2016-01-01 23:17:46 +0900

    あけましておめでとうございます

---
+++ src/main/java/com/google/kozake/sh/tail/TailOperation.java
@@ -0,0 +1,72 @@
+package com.google.kozake.sh.tail;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+public final class TailOperation {
+
+    private final File file;
+
+    public TailOperation(final File file) {
+        this.file = file;
+    }
+
+    public void forwardAndPrint(final Style style, final long off) throws IOException {
+        forwardAndPrint(style, off, System.out);
+    }
+
+    public void forwardAndPrint(final Style style, final long off, final PrintStream out) throws IOException {
+
+        try (FileInputStream fis = new FileInputStream(file)) {
+            forward(fis, style, off);
+
+            int ch;
+            while ((ch = fis.read()) != -1) {
+                out.write(ch);
+            }
+            out.flush();
+        }
+    }
+
+    private void forward(final FileInputStream fis, final Style style, final long off) throws IOException {
+
+        switch(style) {
+            case FBYTES:
+                if (off == 0) {
+                    break;
+                }
+                fis.getChannel().position(Math.min(file.length(), off));
+                break;
+            case FLINES:
+                if (off != 0) {
+                    int ch;
+                    long offset = off;
+                    for (; ; ) {
+                        if ((ch = fis.read()) == -1) {
+                            break;
+                        }
+                        if (ch == '\n' && --offset == 0) {
+                            break;
+                        }
+                    }
+                }
+                break;
+            case RBYTES:
+                if (file.length() >= off) {
+                    fis.getChannel().position(file.length() - off);
+                }
+                break;
+            case RLINES:
+                if (off == 0) {
+                    fis.getChannel().position(file.length());
+                } else {
+                    new FileMap(file, fis).rlines(off);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ src/test/java/com/google/kozake/sh/tail/TailOperationTest.java
@@ -0,0 +1,37 @@
+package com.google.kozake.sh.tail;
+
+import org.junit.Test;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.util.stream.Stream;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class TailOperationTest {
+
+    @Test
+    public void test4MByte() throws IOException {
+
+        File file = new File("src/test/data/ForwardTest4MByte.txt");
+        file.delete();
+
+        String dataStr = Stream.iterate(1, i -> i == 10 ? 1 : i++ % 10).limit(4 << 20).reduce(
+                new StringBuilder(),
+                (buff, i) -> {
+                    buff.append(String.valueOf(i));
+                    return buff;
+                }, StringBuilder::append).toString();
+
+        Files.write(file.toPath(), dataStr.getBytes());
+
+        assertThat(file.length(), is((long) (4 << 20)));
+
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        PrintStream out = new PrintStream(bos);
+
+        TailOperation operation = new TailOperation(file);
+        operation.forwardAndPrint(Style.FBYTES, (4 << 20) - 20);
+        file.delete();
+    }}
